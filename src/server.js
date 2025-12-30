efac35487224ffdf7844c1099a826a485c1790ea
ashwin1473 <ashwindoss0304@gmail.com>
2025-12-30 23:45:28 +0530

    vercel fixed version

--- src/server.js
+++ src/server.js
@@ -5,8 +5,9 @@ const ConnectDB = require("../config/db");
 
 ConnectDB();
 
-const PORT = process.env.PORT || 10000;
+// const PORT = process.env.PORT || 10000;
 
-app.listen(PORT, () => {
-    console.log(`Server is Successfully running on Port ${PORT}`);
-})
+// app.listen(PORT, () => {
+//     console.log(`Server is Successfully running on Port ${PORT}`);
+// })
+module.exports = app;
